f543a66429c2559bbf2e4ea7910659b208543739
Muhammad Umer <mumer1333@gmail.com>
2026-07-19 00:07:16 -0400

    add DynamoDB visitor counter table

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -23,3 +23,14 @@ resource "aws_s3_bucket_public_access_block" "resume" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_dynamodb_table" "visitor_counter" {
+  name         = "visitor-counter"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
