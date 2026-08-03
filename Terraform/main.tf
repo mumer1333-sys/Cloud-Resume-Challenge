62c99d1561b12258ac6dca8762cd6882d3cfe2b9
Muhammad Umer <mumer1333@gmail.com>
2026-08-03 00:30:33 -0500

    Day 1: add API Gateway (HTTP API, integration, route, stage, Lambda permission)

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -89,3 +89,40 @@ resource "aws_lambda_function" "visitor_counter" {
     }
   }
 }
+resource "aws_apigatewayv2_api" "resume_api" {
+  name          = "visitor-counter-api"
+  protocol_type = "HTTP"
+
+  cors_configuration {
+    allow_origins = ["*"]
+    allow_methods = ["GET"]
+    allow_headers = ["content-type"]
+  }
+}
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id                 = aws_apigatewayv2_api.resume_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.visitor_counter.invoke_arn
+  integration_method     = "POST"
+  payload_format_version = "2.0"
+}
+resource "aws_apigatewayv2_route" "get_count" {
+  api_id    = aws_apigatewayv2_api.resume_api.id
+  route_key = "GET /"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+resource "aws_apigatewayv2_stage" "prod" {
+  api_id      = aws_apigatewayv2_api.resume_api.id
+  name        = "prod"
+  auto_deploy = true
+}
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.visitor_counter.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.resume_api.execution_arn}/*/*"
+}
+output "api_invoke_url" {
+  value = aws_apigatewayv2_stage.prod.invoke_url
+}
