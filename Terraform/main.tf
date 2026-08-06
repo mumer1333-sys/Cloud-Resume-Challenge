a10f1808f27de72cc355b71c2e131402a10f6f40
Muhammad Umer <mumer1333@gmail.com>
2026-08-05 21:22:19 -0500

    Day 2: add CloudFront distribution, Origin Access Control, and S3 bucket policy

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -126,3 +126,66 @@ resource "aws_lambda_permission" "api_gw" {
 output "api_invoke_url" {
   value = aws_apigatewayv2_stage.prod.invoke_url
 }
+resource "aws_cloudfront_origin_access_control" "resume_oac" {
+  name                              = "cloud-resume-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+resource "aws_cloudfront_distribution" "resume_cdn" {
+  enabled             = true
+  default_root_object = "index.html"
+
+  origin {
+    domain_name              = aws_s3_bucket.resume.bucket_regional_domain_name
+    origin_id                = "s3-resume-origin"
+    origin_access_control_id = aws_cloudfront_origin_access_control.resume_oac.id
+  }
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods          = ["GET", "HEAD"]
+    target_origin_id       = "s3-resume-origin"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+resource "aws_s3_bucket_policy" "resume_policy" {
+  bucket = aws_s3_bucket.resume.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowCloudFrontServicePrincipal"
+        Effect    = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.resume.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.resume_cdn.arn
+          }
+        }
+      }
+    ]
+  })
+}
